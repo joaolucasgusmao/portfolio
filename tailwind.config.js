fb20b73219546b13a4f8b6ac9b9b1ac5a2470421
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2024-11-14 23:01:47 -0300

    feat: next structure created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        "black-1": "#171923",
+        "black-2": "#252A37"
+      },
+      colors: {
+        "orange": "#eb4432",
+        "white-1": "#ffffff",
+        "white-2": "#e7e8f2",
+        "gray-1": "#b5b5bd",
+        "gray-2": "#93939e",
       },
     },
   },
   plugins: [],
-}
+};
