bdc1ccdfbab5823b05684cf29c8735f5d9bc3379
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2025-07-16 12:35:47 -0300

    feat: update infos

--- src/services/projectsApi.ts
+++ src/services/projectsApi.ts
@@ -1,6 +1,6 @@
 const projectsApi = async () => {
   const response = await fetch(
-    "https://gist.githubusercontent.com/joaolucasgusmao/c53a9cf62716e984e406db48b3b6b789/raw/f8a6486f239846aa3853a6cbd7bdbb5437e24ab9/projects.json"
+    "https://gist.githubusercontent.com/joaolucasgusmao/c53a9cf62716e984e406db48b3b6b789/raw/b592a5ce1e116213f457ac14bd80ad6fa5e60957/projects.json"
   );
 
   if (!response.ok) {
