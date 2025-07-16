cdd89b0e9bf3c1ea055fe07833cfe1c3f7252d5a
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2025-07-16 12:29:50 -0300

    feat: update infos

--- src/services/aboutApi.ts
+++ src/services/aboutApi.ts
@@ -1,6 +1,6 @@
 const aboutApi = async () => {
   const response = await fetch(
-    "https://gist.githubusercontent.com/joaolucasgusmao/b932c05931dbfea0c38d22dd61e4ace1/raw/6630c15b07d91ee3b8eec3b1dfadfed43d7b7d40/about.json"
+    "https://gist.githubusercontent.com/joaolucasgusmao/b932c05931dbfea0c38d22dd61e4ace1/raw/bb0e24b48885ab98176125270977a756f34718c2/about.json"
   );
 
   if (!response.ok) {
--- src/services/projectsApi.ts
+++ src/services/projectsApi.ts
@@ -1,6 +1,6 @@
 const projectsApi = async () => {
   const response = await fetch(
-    "https://gist.githubusercontent.com/joaolucasgusmao/c53a9cf62716e984e406db48b3b6b789/raw/fdf6dfcaff6b2c0405853349ad13ad06a196eb97/projects.json"
+    "https://gist.githubusercontent.com/joaolucasgusmao/c53a9cf62716e984e406db48b3b6b789/raw/f8a6486f239846aa3853a6cbd7bdbb5437e24ab9/projects.json"
   );
 
   if (!response.ok) {
