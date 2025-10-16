8348dd94669a9641a61fab38019951e78f4d1f08
joaolucasgusmao <joaolucasgusmao@hotmail.com>
2025-10-16 13:59:50 -0300

    feat: updated gist

--- src/services/aboutApi.ts
+++ src/services/aboutApi.ts
@@ -1,6 +1,6 @@
 const aboutApi = async () => {
   const response = await fetch(
-    "https://gist.githubusercontent.com/joaolucasgusmao/b932c05931dbfea0c38d22dd61e4ace1/raw/6be6603eb9e4318e8d63c3bb8e8ef313609ec1ce/about.json"
+    "https://gist.githubusercontent.com/joaolucasgusmao/b932c05931dbfea0c38d22dd61e4ace1/raw/dd8ed213e3fa84c76636a22939e39a1716fb6bb0/about.json"
   );
 
   if (!response.ok) {
